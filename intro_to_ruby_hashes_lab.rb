ec732d8d69452f3965b984db9fa9f87af0953f7c
Gene Campbell III <gcampbell391@gmail.com>
2020-02-07 01:56:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,8 +9,8 @@ end
 def monopoly_with_third_tier
       myHash = {:railroads =>{:pieces => 4,
                               :rent_in_dollars => {:one_piece_owned => 25,
-                                                   :two => 2,
-                                                   :three => 3,
+                                                   :two_pieces_owned => 50,
+                                                   :three_pieces_owned =>75               ,
                                                    :four => 4,
                                                   },
                               :names => {          :one => 1,
